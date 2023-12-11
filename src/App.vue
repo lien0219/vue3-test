301ac1cabc991b7bc224f3d1ff5a0edd51dc93c4
lien0219 <lien0001@163.com>
2023-12-11 22:43:02 +0800

    可删除添加的拖拽组件

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@
   <!-- <Map></Map> -->
   <!-- 拖拽组件 -->
   <!-- <TuoDong></TuoDong> -->
-  <Tuo></Tuo>
+  <!-- <Tuo></Tuo> -->
+  <!-- 可删除添加的拖拽组件 -->
+  <editDrag></editDrag>
 </template>
 
 <script setup>
 import Map from "./components/map.vue";
 import TuoDong from "./components/tuodong.vue";
 import Tuo from "./components/tuodongtest.vue";
+import editDrag from "./components/editDrag.vue";
 </script>
 
 <style lang="scss" scoped></style>
